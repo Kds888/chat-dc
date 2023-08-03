6da4cb59bbc1b9509caf6fa2670aa793a0e7ccac
Karandeep Singh <100060333+Kds888@users.noreply.github.com>
2023-08-03 17:56:46 -0400

    Add files via upload

---
+++ dcchat/dcbot/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from . import process
+import json
+
+# Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+def chatbot(request):
+    if request.method == "POST":
+        body_bytes = request.body
+        body_str = body_bytes.decode('utf-8')
+        body_dict = json.loads(body_str)
+        message = body_dict.get('message')
+        bot_response = process.process_input(message)
+        response = JsonResponse({'bot_response': bot_response})
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+    else:
+        response = JsonResponse({'bot_response': 'Get Requests Not Allowed.'})
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
