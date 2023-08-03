41e745a8054a3545be5f77384a1f476be3a07ead
Karandeep Singh <100060333+Kds888@users.noreply.github.com>
2023-08-03 19:02:24 -0400

    Update views.py

--- dcchat/dcbot/views.py
+++ dcchat/dcbot/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 from . import process
 import json
 
+import logging 
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 
 def home(request):
@@ -10,6 +13,7 @@ def home(request):
 
 def chatbot(request):
     if request.method == "POST":
+        logger.info('started service')
         body_bytes = request.body
         body_str = body_bytes.decode('utf-8')
         body_dict = json.loads(body_str)
@@ -19,12 +23,8 @@ def chatbot(request):
         response['Access-Control-Allow-Origin'] = '*'
         return response
     else:
-        body_bytes = request.body
-        body_str = body_bytes.decode('utf-8')
-        body_dict = json.loads(body_str)
-        message = body_dict.get('message')
-        bot_response = process.process_input(message)
-        response = JsonResponse({'bot_response': bot_response})
+        logger.info('in problem')
+        response = JsonResponse({'bot_response': 'Get Requests Not Allowed.'})
         response['Access-Control-Allow-Origin'] = '*'
         return response
 
